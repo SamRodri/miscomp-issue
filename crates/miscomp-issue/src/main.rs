ebbc5df26b28428f8a082b860db4a467f0fad57b
Samuel <sam.rodr.g@gmail.com>
2024-09-26 16:22:34 -0300

    Mac try

---
+++ crates/miscomp-issue/src/main.rs
@@ -0,0 +1,56 @@
+use zng::{
+    event::{event, event_args},
+    prelude::*,
+    view_process::default::*,
+};
+
+fn main() {
+    zng::env::init!();
+    zng::app::print_tracing(tracing::Level::INFO);
+
+    run_same_process(|| {
+        APP.defaults().run(async {
+            test("app-start");
+
+            TEST_EVENT
+                .on_pre_event(app_hn!(|_: &TestArgs, _| {
+                    // this fails (prints None)
+                    test("test-event");
+                }))
+                .perm();
+            TEST_EVENT.notify(TestArgs::now());
+
+            task::deadline(1.secs()).await;
+            APP.exit();
+        });
+    });
+}
+
+pub fn test(ctx: &str) {
+    use zng::layout::*;
+    // (Rect(3240pxx7200px at (-1080px, -2400px)), Rect(1080pxx90px at (0px, 0px))
+    let cull = std::hint::black_box(PxRect::new(
+        PxPoint::new(Px(-1080), Px(-2400)),
+        PxSize::new(Px(3240), Px(7200)),
+    ));
+    let bounds = std::hint::black_box(PxRect::new(
+        PxPoint::new(Px(0), Px(0)),
+        PxSize::new(Px(1080), Px(90)),
+    ));
+    let i = cull.intersection(&bounds);
+    println!("!!: {ctx} {:?}", i);
+}
+
+event_args! {
+    pub struct TestArgs {
+
+        ..
+
+        fn delivery_list(&self, list: &mut UpdateDeliveryList) {
+            list.search_all();
+        }
+    }
+}
+event! {
+    static TEST_EVENT: TestArgs;
+}
