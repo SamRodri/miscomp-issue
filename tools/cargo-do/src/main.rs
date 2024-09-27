8b54a74690ea5a21a284e22ef12fd8a89f7ca331
Samuel <sam.rodr.g@gmail.com>
2024-09-27 15:19:32 -0300

    retry

--- tools/cargo-do/src/main.rs
+++ tools/cargo-do/src/main.rs
@@ -302,7 +302,7 @@ fn update(args: Vec<String>) {
 /// do test-apk
 ///    Called by ci.yml after Android setup
 fn test_apk(_: Vec<String>) {
-    let log = cmd("adb", &["logcat"])
+    let log = cmd("adb", &["logcat", "-b", "main"])
         .stdout(Stdio::piped())
         .spawn()
         .unwrap_or_die("cannot read logcat");
